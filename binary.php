69e71f63a38f5e1fe2462ca6d7bc5bf70d11c2a4
System Administrator <root@MBP-de-Gabriel.home>
2015-11-20 14:37:35 -0200

    binary search

---
+++ binary.php
@@ -0,0 +1,40 @@
+<?php
+
+$data = [3,5,7,9,11,13,15,17,19,21,23,25,27,29];
+echo '$data: ';
+print_r($data);
+echo '</br>';
+echo '</br>';
+
+
+$aux = null;
+
+
+function binarySearch($needed, array $data) {
+
+	$elements = count($data);
+	$first = 0;
+	$last = $elements -1;
+	$middle = (int) (($last - $first) / 2) + $first;
+
+	while ($first <= $last) {
+		if ($data[$middle] < $needed) {
+			$first = $middle + 1;
+		} elseif ($data[$middle] > $needed) {
+			$last = $middle - 1;
+		} else {
+			return $middle;
+		}
+
+		$middle = (int) (($last - $first) / 2) + $first;
+	
+	}
+
+	return -1;
+}
+
+
+echo 'After Binary: ';
+print_r(binarySearch(13, $data));
+echo '</br>';
+?>
